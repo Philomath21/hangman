daa8a0cff747fc9209e2cd714a30bdaeb182c09b
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-08-27 19:35:50 +0530

    converted code into oop
    - created class Game, converted normal code into OOP
    - No new functionality code or variables added to previous code

---
+++ lib/game.rb
@@ -0,0 +1,51 @@
+class Game
+  attr_accessor :secret_word_a, :correct_letters_a, :incorrect_leters_a, :life_i
+
+  def create_dictionary_a
+    # Reading dictiory file as array of words
+    dictionary_a = File.readlines "google-10000-english-no-swears.txt"
+    # Removing \n (whitespaces) from words
+    dictionary_a.map! { |word| word.strip}
+    # Selecting words having 5 to 12 characters
+    dictionary_a.select! { |word| (5..12).include? word.length  }
+  end
+
+  def initialize
+    @dictionary_a = self.create_dictionary_a
+
+    # Selecting a secret word,i.e a random word from dictionary
+    # And storing it in array format
+    @secret_word_a = @dictionary_a.sample.split("")
+
+    # Wrong guesses remaining?
+    @life_i = 5
+
+    # Correct & incorrect guesses made by the player
+    @correct_letters_a = []
+    @incorrect_leters_a = []
+  end
+
+  # Player makes a guess of letter
+  def make_a_guess
+    puts "Guess a letter"
+    while true
+      # making it case insensitive & validating it
+      guess = gets.chomp.downcase
+      break if guess.length == 1 && guess.match?(/[A-Za-z]/)
+      puts "Please enter a valid alphabet"
+    end
+    guess
+  end
+
+  # Check if guess is correct or incorrect
+  def check_guess(guess)
+    if self.secret_word_a.include?(guess)
+      self.correct_letters_a.push(guess)
+      puts "Your guess was correct!"
+    else
+      self.incorrect_leters_a.push(guess)
+      puts "Your guess was incorect"
+    end
+  end
+
+end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,40 +1,4 @@
-# Reading dictiory file as array of words
-dictionary = File.readlines "google-10000-english-no-swears.txt"
+require_relative "game"
 
-# Removing \n (whitespaces) from words
-dictionary.map! { |word| word.strip}
-
-# Selecting words having 5 to 12 characters
-dictionary.select! { |word| (5..12).include? word.length  }
-
-# Selecting a secret word,i.e a random word from dictionary
-# And storing it in array format
-secret_word_a = dictionary.sample.split("")
-
-# Player makes a guess of letter
-# making it case insensitive & validating it
-puts "Guess a letter"
-while true
-  guess = gets.chomp.downcase
-  break if guess.length == 1 && guess.match?(/[A-Za-z]/)
-  puts "Please enter a valid alphabet"
-end
-
-# Wrong guesses remaining?
-life = 5
-
-# Display: _ r o g r a _ _ i n g
-correct_letters_a = []
-# logic
-
-# Display incorrect letters guessed also
-incorrect_leters_a = []
-
-# Check if guess is correct or incorrect
-if secret_word_a.include?(guess)
-  correct_letters_a.push(guess)
-  puts "Your guess was correct!"
-else
-  incorrect_leters_a.push(guess)
-  puts "Your guess was incorect"
-end
+my_game = Game.new
+puts my_game.secret_word_a.join
