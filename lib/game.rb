8a1c1a9d5a6810b3f924709b1e0d5f0d42723271
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-08-27 21:31:15 +0530

    bug fix: repeated letter guess is now not taken

--- lib/game.rb
+++ lib/game.rb
@@ -3,19 +3,19 @@ class Game
 
   def create_dictionary_a
     # Reading dictiory file as array of words
-    dictionary_a = File.readlines "google-10000-english-no-swears.txt"
+    dictionary_a = File.readlines 'google-10000-english-no-swears.txt'
     # Removing \n (whitespaces) from words
-    dictionary_a.map! { |word| word.strip}
+    dictionary_a.map!(&:strip)
     # Selecting words having 5 to 12 characters
-    dictionary_a.select! { |word| (5..12).include? word.length  }
+    dictionary_a.select! { |word| (5..12).include? word.length }
   end
 
   def initialize
-    @dictionary_a = self.create_dictionary_a
+    @dictionary_a = create_dictionary_a
 
     # Selecting a secret word,i.e a random word from dictionary
     # And storing it in array format
-    @secret_word_a = @dictionary_a.sample.split("")
+    @secret_word_a = @dictionary_a.sample.split('')
 
     # Wrong guesses remaining?
     @life_i = 5
@@ -27,34 +27,48 @@ class Game
 
   # Player makes a guess of letter
   def make_a_guess
-    puts "Guess a letter"
+    puts 'Guess a letter'
     while true
       # making it case insensitive & validating it
       guess = gets.chomp.downcase
-      break if guess.length == 1 && guess.match?(/[A-Za-z]/)
-      puts "Please enter a valid alphabet"
+      if guess.length == 1 && guess.match?(/[A-Za-z]/)
+        break unless (correct_letters_a + incorrect_letters_a).include? guess
+
+        puts 'You have already entered this alphabet, please enter a new alphabet'
+      else
+        puts 'Please enter a valid alphabet'
+      end
     end
     guess
   end
 
   # Check if guess is correct or incorrect
   def check_guess(guess)
-    if self.secret_word_a.include?(guess)
-      self.correct_letters_a.push(guess)
-      puts "Your guess was correct!"
+    if secret_word_a.include?(guess)
+      correct_letters_a.push(guess)
+      puts 'Your guess was correct!'
     else
-      self.incorrect_letters_a.push(guess)
-      puts "Your guess was incorect"
+      incorrect_letters_a.push(guess)
+      puts 'Your guess was incorect'
     end
   end
 
+  # Check for a win
+  def game_won?
+    secret_word_a.each do |letter|
+      return false unless correct_letters_a.include?(letter)
+    end
+    true
+  end
+
+  # Print revealed letters in secret word & incorrect guesses
   def to_s
-    secret_word = ""
-    self.secret_word_a.each do |letter|
-      secret_word += self.correct_letters_a.include?(letter) ?  "#{letter} " : "_ "
+    secret_word = ''
+    secret_word_a.each do |letter|
+      secret_word += correct_letters_a.include?(letter) ? "#{letter} " : '_ '
     end
 
     "Secret word: #{secret_word}
-Incorrect letters are: #{self.incorrect_letters_a.join(", ")}"
+Incorrect letters are: #{incorrect_letters_a.join(', ')}"
   end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,13 @@
-require_relative "game"
+require_relative 'game'
 
 # Gameplay logic
 def play_game
-
-
   my_game = Game.new
-
-  while true
+  until my_game.game_won?
     puts my_game
     guess = my_game.make_a_guess
     my_game.check_guess(guess)
-    puts " "
+    puts ' '
   end
 end
 
